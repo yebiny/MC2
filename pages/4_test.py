30dd3e6687e804428c61faad4dc252bbcb188d7f
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-02 02:56:16 +0900

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -4,15 +4,4 @@ from google.oauth2 import service_account
 import json
 
 key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
-
-# Then get the data at that reference.
-doc = doc_ref.get()
-
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
