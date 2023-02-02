4cbf533862de16200deed31c1a2ca3ea5f9b1c4c
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-02 20:28:33 +0900

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -10,22 +10,16 @@ st.text('creds')
 db = firestore.Client(credentials=creds, project="Monchaton2022")
 st.text('db')
 
-doc = db.collection('user001').document("2023_02_02_20_02_45").get()
-st.text(doc.to_dict())
+doc = db.collection('user001').document("2023_02_02_20_02_45")
+st.text(doc)
 
-# For a reference to a collection, we use .stream() instead of .get()
-#for doc in collection.stream():
-#	st.write("The id is: ", doc.id)
-#	st.write("The contents are: ", doc.to_dict())
-  
-  
-#st.text(collections)
-#ref = db.document('Videos')
-#st.text('ref')
-#st.text(ref)
-#for collection in collections:
-#    st.text(collections)
-#    for doc in collection.stream():
-#        st.text(f'{doc.id} => {doc.to_dict()}')
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())(
+
 
 
