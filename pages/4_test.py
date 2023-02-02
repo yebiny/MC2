6c90be721733f1270515caf6881701722cbdfe01
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-02 21:58:12 +0900

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -2,10 +2,6 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 import json
-import urllib.request
-
-def save_video(video_url, save_name) :
-    urllib.request.urlretrieve(video_url, save_name)    
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
@@ -18,7 +14,6 @@ for doc in collection.stream():
   post = doc.to_dict()
   video_name = post['File_title']
   url = post['URL']
-  save_video(url, f'./{video_name}')
   
   st.subheader(f"Date: {video_name}")
   st.write(f":link: [{url}]({url})")
