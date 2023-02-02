be5b3aa7787a15c9b256b5b4c80a6bf14357bed9
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-02 21:10:06 +0900

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -4,20 +4,16 @@ from google.oauth2 import service_account
 import json
 
 key_dict = json.loads(st.secrets["textkey"])
-st.text('key')
 creds = service_account.Credentials.from_service_account_info(key_dict)
-st.text('creds')
 db = firestore.Client(credentials=creds)
-st.text('db')
-st.text(dir(db))
-
 collection = db.collection("user001")
-st.text(dir(collection))
-st.text(collection.stream())
+
+st.title('반려캠 ')
+
 for doc in collection.stream():
   post = doc.to_dict()
   vidoe_name = post['File_title']
-  url = post['URL'][4:]
+  url = post['URL'][5:]
   
   st.subheader(f"Date: {vidoe_name}")
   st.write(f":link: [{url}]({url})")
