5406ada5e3bc9c260aafa784afd59faa72206490
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-02 22:50:43 +0900

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -4,7 +4,8 @@ from google.oauth2 import service_account
 import json
 import cv2
 import numpy as np
-import cv2
+import datetime
+
 
 def get_frame_from_url(url):
   cap = cv2.VideoCapture(url)
@@ -19,32 +20,38 @@ def get_frame_from_url(url):
         
   return loadedImage
 
-
+# 파이어베이스 db에서 정보 가져오기
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds)
 collection = db.collection("user001")
 
-st.title('반려캠 ')
 
-ds = []
+
+st.title('저장된 영상 플레이')
+y, m, d = st.date_input(
+    "영상을 불러올 날짜를 선택하세요.",
+    datetime.date(2023, 1, 1))
+
+
+start_date = False
 for doc in collection.stream():
-  post = doc.to_dict()
-  video_name = post['File_title']
-  url = post['URL']
-  loadedImage = get_frame_from_url(url)
-  ds.append([video_name, url, loadedImage])
- 
-for (video_name, url, loadedImage) in ds: 
-  st.subheader(video_name)
-  col1, col2, col3 = st.columns(3)
-  with col1:
-    st.text("장소:")
-  with col2:
-    if loadedImage is not None:
-      st.image(loadedImage)
-  with col3:
-    if st.button('Play', key=video_name):
-      st.write('play!')
+  docId = doc.id()
+  if (y, m, d) == docId[:3]: 
+    url = post['URL']
+    st.text(url)
+  #loadedImage = get_frame_from_url(url)
+  
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.text("장소:")
+with col2:
+  st.text('이미지')
+  #if loadedImage is not None:
+  #  st.image(loadedImage)
+with col3:
+  if st.button('Play', key=video_name):
+    st.write('play!')
 
     
