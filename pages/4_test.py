b74ba2fc83101e034c8f1b7112d0c336d3b54c57
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-02 22:06:45 +0900

    Update 4_test.py

--- pages/4_test.py
+++ pages/4_test.py
@@ -18,10 +18,5 @@ for doc in collection.stream():
   
   st.subheader(f"Date: {video_name}")
   cap = cv2.VideoCapture(url)
-  while(True):
-    ret, frame = cap.read()
-    if frame is not None:
-        st.image(frame)
-        break
-    else:
-        break
+  ret, frame = cap.read()
+  st.image(frame)
