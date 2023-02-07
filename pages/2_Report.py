f1ade354295261a75be00c508e20777a09cf047d
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-07 20:07:40 +0900

    Update 2_Report.py

--- pages/2_Report.py
+++ pages/2_Report.py
@@ -83,18 +83,19 @@ def main():
         st.write(f'{h}시 {mi}분 {se}초')
         st.text(f'분석: {analyzed}')
      
-        
-    for idx, (h, mi, se, url, analyzed) in enumerate(ds):
-        video_info = get_video_info(url)
-        st.write(video_info)
-        
+    
+    #for idx, (h, mi, se, url, analyzed) in enumerate(ds):
+
+    url = ds[0][3]
+    video_info = get_video_info(url)
+    st.write(url)
+    st.write(video_info)
+
     save_path = './tmp-videos/tmp.mp4'
     cvt_path = './tmp-videos/cvt.mp4'
     detect_video( model, video_info, save_path)
     subprocess.call(f"ffmpeg -y -i {save_path} -c:v libx264 {cvt_path}", shell=True)
     
-    
-    st.video(cvt_path)    
 
     
 
