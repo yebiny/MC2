e428f82d0db33a1c4db315701b32f0bdb9db5e30
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-07 20:41:34 +0900

    Update 2_Report.py

--- pages/2_Report.py
+++ pages/2_Report.py
@@ -97,7 +97,7 @@ def main():
         detect_video( model, video_info, save_path)
         subprocess.call(f"ffmpeg -y -i {save_path} -c:v libx264 {cvt_path}", shell=True)
     
-
+        st.video(cvt_path)
     
 
 
