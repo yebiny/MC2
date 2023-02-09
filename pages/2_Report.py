bb627ae2bc434b0c0ab37830b49dba6b7d16e229
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-09 17:57:19 +0900

    Update 2_Report.py

--- pages/2_Report.py
+++ pages/2_Report.py
@@ -125,6 +125,9 @@ def main():
             with c2:
                 play_button =  st.button('Play', key=doc_id)
                 if play_button: target_video = f'./tmp-videos/{doc_id}.mp4'.replace('.mp4', '-cvt.mp4')
+            with c3:
+                txt = f'<p style="font-family:sans-serif; color:{color}; font-size: 22.5px;">{analyzed}</p>'
+                st.markdown(txt, unsafe_allow_html=True)
 
 	# 분석 버튼 누르면 분석 진행
         if analyze_button: 
