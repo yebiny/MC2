771543bbee441d682aaa592eeff6d938ea8b3b18
Nibey <50440208+yebiny@users.noreply.github.com>
2023-02-09 16:04:55 +0900

    Update 2_Report.py

--- pages/2_Report.py
+++ pages/2_Report.py
@@ -4,11 +4,12 @@ from google.oauth2 import service_account
 import json
 import cv2
 import numpy as np
-import datetime
+import datetime, pytz
 import shutil
 from scripts.tflite_lib import *
 import io
 import subprocess
+
 
 def get_video_info(video_path):
     # so now we can process it with OpenCV functions
@@ -76,6 +77,9 @@ def main():
     
     # 날짜 선택
     st.title('리포트')
+    KST = pytz.timezone('Asia/Seoul')
+    now = dt.now(KST)
+    st.text(now)
     select_y, select_m, select_d = 2023, 2, 1
     date_input= st.date_input("분석할 날짜를 선택하세요.", datetime.date(select_y, select_m, select_d) )
     select_y, select_m, select_d = str(date_input).split('-') 
